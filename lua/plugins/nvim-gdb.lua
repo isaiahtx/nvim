494a92794f1d19eeb1f4b36fa02e617f0a174edb
Isaiah Dailey <isaiahdaileytx@gmail.com>
2024-01-30 13:52:36 -0800

    !!

---
+++ lua/plugins/nvim-gdb.lua
@@ -0,0 +1,11 @@
+return {
+  -- add nvim-gdb
+  {
+    "sakhnik/nvim-gdb",
+    -- Add any additional options or configurations here
+    -- For example, you can pass setup configurations inside an 'opts' table
+    -- opts = {
+    --   ... your configurations ...
+    -- },
+  },
+}
