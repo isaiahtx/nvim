eeab9b44297f593d0be3424d55e897096368bf0e
Isaiah Dailey <isaiahdaileytx@gmail.com>
2024-02-09 15:30:06 -0800

    !

---
+++ lua/plugins/vim-visual-multi.lua
@@ -0,0 +1,11 @@
+return {
+  -- add nvim-gdb
+  {
+    "mg979/vim-visual-multi",
+    -- Add any additional options or configurations here
+    -- For example, you can pass setup configurations inside an 'opts' table
+    -- opts = {
+    --   ... your configurations ...
+    -- },
+  },
+}
