055b64bb85316a530e83e65bc92e5317d6532176
Isaiah Dailey <isaia@Tchaikovsky.local>
2024-02-18 14:43:08 -0800

    !!!

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -6,8 +6,16 @@ return {
       vim.g["tex_flavor"] = "latex"
       vim.g["vimtex_quickfix_mode"] = 0
       vim.g["vimtex_syntax_conceal_disable"] = 0
-      vim.g.vimtex_view_method = "zathura"
-      vim.g["vimtex_view_general_options"] = "-reuse-instance @pdf"
+
+      if vim.fn.system("uname") == "Darwin\n" then
+        vim.g.vimtex_view_method = "skim"
+        vim.g.vimtex_compiler_progname = "nvr"
+        vim.g["vimtex_view_general_options"] = "-r @line @pdf @src"
+      else
+        vim.g.vimtex_view_method = "zathura"
+        vim.g["vimtex_view_general_options"] = "-reuse-instance @pdf"
+      end
+
       vim.g["vimtex_syntax_conceal"] = {
         accents = 1,
         cites = 1,
@@ -15,7 +23,7 @@ return {
         greek = 1,
         math_bounds = 0,
         math_delimiters = 0,
-        math_fracs = 0,
+        math_fracs = 1,
         math_super_sub = 1,
         math_symbols = 1,
         sections = 1,
