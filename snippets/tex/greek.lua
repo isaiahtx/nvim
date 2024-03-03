e6741750786db1acceaaeb7ad53d4e9bdbc6daad
Isaiah Dailey <isaiahdaileytx@gmail.com>
2024-03-02 19:26:49 -0800

    iota

--- snippets/tex/greek.lua
+++ snippets/tex/greek.lua
@@ -1,8 +1,8 @@
 local ls = require("luasnip")
 local s = ls.snippet
 local sn = ls.snippet_node
-local t = ls.text_node
 local i = ls.insert_node
+local t = ls.text_node
 local f = ls.function_node
 local d = ls.dynamic_node
 local fmt = require("luasnip.extras.fmt").fmt
@@ -52,6 +52,11 @@ return {
   s(
     { trig = ";ve", snippetType = "autosnippet", wordTrig = false },
     { t("\\varepsilon") },
+    { condition = math }
+  ),
+  s(
+    { trig = ";i", wordTrig = false, snippetType = "autosnippet" },
+    { t("\\iota") },
     { condition = math }
   ),
   s(
