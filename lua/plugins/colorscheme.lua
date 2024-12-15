bd7b8f35700138c53ca29c7282d4f8042f0d4d62
Isaiah Dailey <isaiahdaileytx@gmail.com>
2024-12-15 00:07:49 -0600

    colors!

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,21 @@
 return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+
   { "pacokwon/onedarkhc.vim" },
+  { "rebelot/kanagawa.nvim" },
+  { "navarasu/onedark.nvim" },
+  { "scottmckendry/cyberdream.nvim" },
+  { "Tsuzat/NeoSolarized.nvim" },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedarkhc",
+      colorscheme = "NeoSolarized",
     },
   },
 }
