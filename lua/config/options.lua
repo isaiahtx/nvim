76e070723dadf71db30ce6f32171998e797de941
Isaiah Dailey <isaiah.dailey@magnetar.com>
2026-01-27 09:37:22 -0600

    update for work laptop

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,13 +1,1 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
--- So yanking/deleting does not cause text to enter my system clipboard
-vim.opt.clipboard = ""
-
-vim.opt.colorcolumn = "80"
-vim.opt.wrap = true
-vim.opt.linebreak = true
-vim.opt.breakindent = true
-vim.opt.fo = "jntrocqp"
-vim.g.lazyvim_cmp = "nvim-cmp"
+vim.g.python3_host_prog = vim.fn.expand("~/.venvs/nvim/bin/python")
